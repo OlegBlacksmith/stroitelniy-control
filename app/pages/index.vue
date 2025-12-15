1829b100151effae7b472051af6c1e8e43200808
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-12-16 00:33:57 +0400

    add: pages

---
+++ app/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppStartSection from '~/components/sections/AppStartSection.vue';
+import AppServiceSection from '~/components/sections/AppServiceSection.vue';
+import AppAboutCompanySection from '~/components/sections/AppAboutCompanySection.vue';
+import AppPartnersSection from '~/components/sections/AppPartnersSection.vue';
+import AppBimSection from '~/components/sections/AppBimSection.vue';
+import AppLawsSection from '~/components/sections/AppLawsSection.vue';
+import AppDocumentsSection from '~/components/sections/AppDocumentsSection.vue';
+</script>
+
+<template>
+  <div>
+    <AppStartSection />
+    <AppServiceSection />
+    <AppAboutCompanySection />
+    <AppPartnersSection />
+    <AppBimSection />
+    <AppLawsSection />
+    <AppDocumentsSection />
+  </div>
+</template>
