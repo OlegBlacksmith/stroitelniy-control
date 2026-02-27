b113b148017d0fcb1225221b3cab897f7c143ed1
evelindev <evelinvigberg@gmail.com>
2026-02-27 21:09:00 +0400

    feature(fix): добавлены эмиты по клику, изменены стили, добавлены стили для мобилки

--- app/components/AppOrderButton.vue
+++ app/components/AppOrderButton.vue
@@ -1,34 +1,76 @@
 <script setup lang="ts">
 defineProps<{
-  buttonText?: string,
-  padding?: string
-}>()
+  buttonText?: string;
+  padding?: string;
+  fontSize?: string;
+}>();
+
+defineEmits<{
+  (e: "click"): void;
+}>();
 </script>
 
 <template>
-    <button :style="{ padding: padding || '20px 50px' }" class="order-button">
-        <p class="button-text">{{ buttonText }}</p>
-    </button>
+  <button class="order-button" @click="$emit('click')">
+    <p class="button-text">{{ buttonText }}</p>
+  </button>
 </template>
 
 <style scoped>
-.order-button{
-    border-radius: 8px;
-    background-color: #2D56EC;
-    border: none;
-    cursor: pointer;
-    transition: 0.3s;
-    box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.3);
+.order-button {
+  padding: 20px 50px;
+  border-radius: 8px;
+  background-color: #2d56ec;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s;
+  box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.3);
 }
 
-.order-button:hover{
-    background-color: #4c70f4;
+.order-button:hover {
+  background-color: #4c70f4;
 }
 
-.button-text{
-    font-family: 'Roboto', sans-serif;
-    font-size: 18px;
+.button-text {
+  font-family: "Roboto", sans-serif;
+  font-size: 24px;
+  color: white;
+  font-weight: 900;
+}
+
+/* ПЛАНШЕТ (992px-) */
+@media (max-width: 991px) {
+}
+
+/* МОБИЛЬНЫЕ (767px-) — СКРЫТЬ КАРТИНКУ */
+@media (max-width: 767px) {
+}
+
+/* МАЛЕНЬКИЕ ТЕЛЕФОНЫ (479px-) */
+@media (max-width: 479px) {
+  .order-button {
+    padding: 20px 50px;
+  }
+
+  .button-text {
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
     color: white;
     font-weight: 900;
+  }
+}
+
+/* КРОХОТНЫЕ */
+@media (max-width: 359px) {
+  .order-button {
+    padding: 10px 30px;
+  }
+
+  .button-text {
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    color: white;
+    font-weight: 900;
+  }
 }
 </style>
--- app/components/AppPartnerButton.vue
+++ app/components/AppPartnerButton.vue
@@ -1,35 +1,43 @@
 <script setup lang="ts">
 defineProps<{
-    buttonText?: string,
-    padding?: string,
-    fontSize?: string
-}>()
+  buttonText?: string;
+  padding?: string;
+  fontSize?: string;
+}>();
+
+defineEmits<{
+  (e: "click"): void;
+}>();
 </script>
 
 <template>
-    <button :style="{ padding: padding || '20px 50px', fontSize: fontSize }" class="order-button">
-        <p class="button-text">{{ buttonText }}</p>
-    </button>
+  <button
+    :style="{ padding: padding || '20px 50px', fontSize: fontSize || '18px' }"
+    class="order-button"
+    @click="$emit('click')"
+  >
+    <p class="button-text">{{ buttonText }}</p>
+  </button>
 </template>
 
 <style scoped>
-.order-button{
-    border-radius: 8px;
-    background-color: #2D56EC;
-    border: none;
-    cursor: pointer;
-    transition: 0.3s;
-    box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.3);
+.order-button {
+  border-radius: 8px;
+  background-color: #2d56ec;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s;
+  box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.3);
 }
 
-.order-button:hover{
-    background-color: #4c70f4;
+.order-button:hover {
+  background-color: #4c70f4;
 }
 
-.button-text{
-    font-family: 'Roboto', sans-serif;
-    font-size: 18px;
-    color: white;
-    font-weight: 900;
+.button-text {
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  color: white;
+  font-weight: 900;
 }
 </style>
